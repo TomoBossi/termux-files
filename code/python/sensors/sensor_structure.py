fbe5942378566a948f559801d44ff755223dafed
TomoBossi <tomasbossi97@gmail.com>
2024-07-20 14:12:07 -0300

    add python script for background sensor data parsing

---
+++ code/python/sensors/sensor_structure.py
@@ -0,0 +1,29 @@
+from numpy import zeros
+
+sensor_structure = {                         # Usability (OK/MID/BAD) / Values / Misc
+    'ACCELEROMETER': zeros(3),               # OK
+    'MAGNETOMETER': zeros(3),                # OK
+    'ORIENTATION': zeros(3),                 # BAD / / Unusable except through -a option because termux interprets ORIENTATION as DEVICE_ORIENTATION
+    'GYROSCOPE': zeros(3),                   # OK
+    'LIGHT': zeros(1),                       # OK
+    'PROXIMITY': zeros(1),                   # OK / X; X = 0, 1 / Near, far
+    'GRAVITY': zeros(3),                     # OK / / Works great, does not detect orientation around Z axis
+    'LINEARACCEL': zeros(3),                 # OK
+    'ROTATION_VECTOR': zeros(5),             # BAD / Stuck at [0, 0, 0, 1] / Possibly broken
+    'UNCALI_MAG': zeros(6),                  # MID
+    'GAME_ROTATION_VECTOR': zeros(4),        # BAD / Stuck at [0, 0, 0, 1] / Possibly broken
+    'UNCALI_GYRO': zeros(6),                 # MID
+    'SIGNIFICANT_MOTION': zeros(1),          # BAD / None
+    'STEP_DETECTOR': zeros(1),               # MID / None until step detected, becomes single 1
+    'STEP_COUNTER': zeros(1),                # BAD / None until step detected, becomes single counter / Doesn't seem to work properly
+    'GEOMAGNETIC_ROTATION_VECTOR': zeros(5), # MID / / Somewhat noisy
+    'TILT_DETECTOR': zeros(1),               # BAD / None
+    'WAKE_GESTURE': zeros(1),                # BAD / None
+    'DEVICE_ORIENTATION': zeros(1),          # OK / X; X = 0, 1, 2, 3 / 90° counterclockwise rotation
+    'UNCALI_ACC': zeros(6),                  # MID
+    'SAR': zeros(16),                        # MID / [X, 0, ..., 0]; X = 3, 2, ? / Very close a living body, not too close, ?
+    'CHOPCHOP_GESTURE': zeros(16),           # MID / None until chopchop detected, becomes [1, 0, ..., 0]
+    'CAMERA_GESTURE': zeros(16),             # MID / None until camera shake detected, becomes [1, 0, ..., 0]                 
+    'SIGNIFICANT_MOVE': zeros(1),            # BAD / None
+    'STEP_DETECTOR_WAKEUP': zeros(1),        # MID / None until step detected, becomes single 1 / Seems the same as STEP_DETECTOR
+}
---
+++ code/python/sensors/update_sensor_data.py
@@ -0,0 +1,76 @@
+import subprocess
+import threading
+import time
+import sys
+import re
+import curses
+
+from sensor_structure import sensor_structure
+
+global_sensor_data = {}
+global_stdout_line_idx = 0
+
+def global_sensor_data_init(sensors: list[str]):
+    global global_sensor_data
+    for sensor in sensors:
+        global_sensor_data[sensor] = sensor_structure[sensor]
+
+def execute(cmd: list[str]):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+
+def poll(sensors: list[str], delay: int):
+    cmd = ['termux-sensor', '-s', f'{",".join(sensors)}', '-d', f'{delay}']
+    for stdout_line in execute(cmd):
+        yield stdout_line
+
+def update_global_sensor_data(sensors: list[str], delay: int):
+    global global_sensor_data
+    global global_stdout_line_idx
+    global_sensor_data_init(sensors)
+
+    sensor = None
+    sensor_line = False
+    sensor_line_idx = -1
+    for line in poll(sensors, delay):
+        if not sensor_line and '"' in line:
+            sensor = line.split('"')[1]
+            sensor_line = True
+            sensor_line_idx = 0
+
+        if sensor_line and sensor_line_idx > 1:
+            if sensor_line_idx < sensor_structure[sensor].size + 2:
+                global_sensor_data[sensor][sensor_line_idx - 2] = float(re.findall(r'[+-]?\d+(?:\.\d+)?', line)[0])
+            else:
+                sensor_line = False
+        
+        sensor_line_idx += 1
+        global_stdout_line_idx += 1
+
+def main():
+    if len(sys.argv) < 3:
+        print(f'Usage: python {sys.argv[0]} <delay (ms)> sensor_1 sensor_2 ... sensor_n')
+        return
+
+    delay = int(sys.argv[1])
+    sensors = [s.upper() for s in sys.argv[2:]]
+    stdscr = curses.initscr()
+
+    cmd_thread = threading.Thread(target=update_global_sensor_data, args=(sensors, delay))
+    cmd_thread.start()
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr(0, 0, '{')
+        for idx, key in enumerate(global_sensor_data.keys(), start=1):
+            stdscr.addstr(idx, 0, f'\t{key}: {list(global_sensor_data[key])}')
+        stdscr.addstr(idx+1, 0, '}')
+        stdscr.refresh()
+        time.sleep(0.3)
+    
+    cmd_thread.join()
+
+if __name__ == '__main__':
+    main()
